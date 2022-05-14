bbdecc6bc987d68d354560405b7223a609b2dcec
rkddnr330 <rkddnr330@gmail.com>
2022-05-14 18:12:48 +0900

    start new project: JSONDemo1

---
+++ JSONDemo1/JSONDemo1/JSONDemo1App.swift
@@ -0,0 +1,17 @@
+//
+//  JSONDemo1App.swift
+//  JSONDemo1
+//
+//  Created by Park Kangwook on 2022/05/14.
+//
+
+import SwiftUI
+
+@main
+struct JSONDemo1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
