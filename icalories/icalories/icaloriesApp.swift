5c958d349d448cc833e5a3d52a3170d07fca7f4a
rkddnr330 <rkddnr330@gmail.com>
2022-05-16 10:53:58 +0900

    start new project: icalories

---
+++ icalories/icalories/icaloriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  icaloriesApp.swift
+//  icalories
+//
+//  Created by Park Kangwook on 2022/05/16.
+//
+
+import SwiftUI
+
+@main
+struct icaloriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
