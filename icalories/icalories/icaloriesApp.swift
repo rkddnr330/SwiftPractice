62584c0e6e95a2dfe687286e14a906f7b4759977
rkddnr330 <rkddnr330@gmail.com>
2022-05-16 19:34:14 +0900

    [icalories] feat: Injecting the Database

--- icalories/icalories/icaloriesApp.swift
+++ icalories/icalories/icaloriesApp.swift
@@ -9,9 +9,13 @@ import SwiftUI
 
 @main
 struct icaloriesApp: App {
+    ///@StateObject로 가져온다. DataController 파일에 있는 DataController class를 가져온다는 의미로 DataController() (이니셜라이저까지)
+    @StateObject private var dataController = DataController()
     var body: some Scene {
         WindowGroup {
             ContentView()
+                ///모든 곳에서 이 데이터를 쓸 수 있게하는 코드
+                .environment(\.managedObjectContext, dataController.container.viewContext)
         }
     }
 }
