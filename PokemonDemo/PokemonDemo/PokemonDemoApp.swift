0f6b06c391a58f3bce62f436601c0955bc68eabd
rkddnr330 <rkddnr330@gmail.com>
2022-05-18 10:32:44 +0900

    [PokemonDemo] initial commit

---
+++ PokemonDemo/PokemonDemo/PokemonDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonDemoApp.swift
+//  PokemonDemo
+//
+//  Created by Park Kangwook on 2022/05/18.
+//
+
+import SwiftUI
+
+@main
+struct PokemonDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
