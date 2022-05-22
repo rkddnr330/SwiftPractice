3bfa58799f1fa4f98ccda40b0366eb4714473348
rkddnr330 <rkddnr330@gmail.com>
2022-05-22 18:41:59 +0900

    [PokemonDemo] doc: Helpers 설명 추가

--- PokemonDemo/PokemonDemo/Helpers/Helpers.swift
+++ PokemonDemo/PokemonDemo/Helpers/Helpers.swift
@@ -29,7 +29,20 @@ extension Bundle {
         
         return loadedData
     }
-    
+    ///decode 함수의 전체적인 흐름
+    ///let url = self.url(forResource: file, withExtension: nil)
+    ///let data = try? Data(contentsOf: url)
+    ///let decoder = JSONDecoder()
+    ///let loadedData = try? decoder.decode(T.self, from: data)
+    ///return loadedData
+    ///만약 순서에서 하나라도 빠진 게 존재하면 fatal Error 띄워라! 를 guard문과 옵셔널 바인딩을 통해 구현함
+
+    ///FetchData라는 이름의 함수에 제네릭 사용
+    ///<T: Decodable> 에서 T : 플레이스 홀더, Decodable
+    ///매개변수 종류 : url, model, completion, failure
+    ///매개변수 타입 설정 시 @escaping() -> () : escaping 클로저 사용
+    ///-목적 : 함수를 외부에서 사용하겠다 (원래 안됨. 함수가 선언된 곳에서만 사용될 수 있는데 외부에서도 사용하고 싶음.
+    ///-비동기 처리 HTTP 요청 처리시 자주 사용된다
     func FetchData<T: Decodable>(url: String, model: T.Type, completion: @escaping(T) -> (), failure: @escaping(Error) -> ()) {
         guard let url = URL(string: url) else {return}
         
@@ -50,11 +63,3 @@ extension Bundle {
         }.resume()
     }
 }
-///decode 함수의 전체적인 흐름
-///let url = self.url(forResource: file, withExtension: nil)
-///let data = try? Data(contentsOf: url)
-///let decoder = JSONDecoder()
-///let loadedData = try? decoder.decode(T.self, from: data)
-///return loadedData
-///만약 순서에서 하나라도 빠진 게 존재하면 fatal Error 띄워라! 를 guard문과 옵셔널 바인딩을 통해 구현함
-
