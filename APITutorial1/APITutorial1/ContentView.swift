da8f6ff0c0405048862c2439f0d36a65a08b733a
rkddnr330 <rkddnr330@gmail.com>
2022-05-25 20:49:56 +0900

    [APITutorial1] feat: finish (API, decode, async, await)

--- APITutorial1/APITutorial1/ContentView.swift
+++ APITutorial1/APITutorial1/ContentView.swift
@@ -7,10 +7,57 @@
 
 import SwiftUI
 
+///Quote 타입 생성
+struct Quote: Codable {
+    var id: Int
+    var quote_id: String
+    var quote: String
+    var author: String
+    var series: String
+}
+
 struct ContentView: View {
+    @State private var quotes = [Quote]()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List(quotes, id : \.quote_id) { quote in
+                VStack {
+                    Text(quote.author)
+                        .font(.headline)
+                        .foregroundColor(.blue)
+                    Text(quote.quote)
+                        .font(.subheadline)
+                        .foregroundColor(.secondary)
+                }
+                
+            }
+            .navigationTitle("Quotes")
+            .task {
+                await fetchData()
+            }
+        }
+    }
+    
+    ///API 활용해서 data 가져오는 함수
+    func fetchData() async {
+        ///create url
+        guard let url = URL(string: "https://www.breakingbadapi.com/api/quotes") else {
+            print("url is invalid. try again!")
+            return
+        }
+        ///fetch data from the url
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            
+            ///decode that data
+            if let decodedResponse = try? JSONDecoder().decode([Quote].self, from:data) {
+                quotes = decodedResponse
+            }
+        } catch {
+            print("data is invalid")
+        }
+        
     }
 }
 
