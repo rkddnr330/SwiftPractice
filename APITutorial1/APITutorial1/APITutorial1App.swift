6a7ba5ea6115e5e3591ac1cbb7de1e1fd195f575
rkddnr330 <rkddnr330@gmail.com>
2022-05-25 19:34:49 +0900

    [APITutorial1] initial commit

---
+++ APITutorial1/APITutorial1/APITutorial1App.swift
@@ -0,0 +1,17 @@
+//
+//  APITutorial1App.swift
+//  APITutorial1
+//
+//  Created by Park Kangwook on 2022/05/25.
+//
+
+import SwiftUI
+
+@main
+struct APITutorial1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ APITutorial1/APITutorial1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  APITutorial1
+//
+//  Created by Park Kangwook on 2022/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
