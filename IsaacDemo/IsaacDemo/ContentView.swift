7871ee90f82b4721721f2f05feefbc3901b43ced
rkddnr330 <rkddnr330@gmail.com>
2022-05-26 12:17:30 +0900

    [IsaacDemo] feat: load words from API

--- IsaacDemo/IsaacDemo/ContentView.swift
+++ IsaacDemo/IsaacDemo/ContentView.swift
@@ -7,16 +7,42 @@
 
 import SwiftUI
 
-
-
 struct ContentView: View {
     
- 
+    @State private var words = [String]()
     
     var body: some View {
-        Text("Hello world")
+        NavigationView {
+            List(words, id:\.self) { item in
+                Text(item)
+                    .padding()
+            }
+            .navigationTitle("Words List")
+            .task{
+                await fetchData()
+            }
+        }
     }
     
+    func fetchData() async {
+        //create url
+        guard let url = URL(string: "https://random-word-api.herokuapp.com/word?number=20") else {
+            print("url is invalid!")
+            return
+        }
+        
+        //fetch the data from url
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            //decode that data
+            if let decodedResponse = try? JSONDecoder().decode([String].self, from: data) {
+                words = decodedResponse
+            }
+        } catch {
+            print("data is invalid!")
+        }
+        
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
