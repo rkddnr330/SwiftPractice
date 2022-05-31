8716f8bc802b378818081b1bac81dc1588115c6d
rkddnr330 <rkddnr330@gmail.com>
2022-05-31 20:35:19 +0900

    [IsaacDemo] feat: change from Slider to TextField & create Alert

--- IsaacDemo/IsaacDemo/IntroView.swift
+++ IsaacDemo/IsaacDemo/IntroView.swift
@@ -9,18 +9,37 @@ import SwiftUI
 
 struct IntroView: View {
     @State private var wordsCount: Double = 0
+    @State private var showingAlert = false
+    
     var body: some View {
         NavigationView {
             VStack {
-                Text("단어 개수: \(Int(wordsCount))")
+                Text("단어 개수를 입력하세요 🥸")
                     .font(.title)
-                Slider(value: $wordsCount, in: 0...15, step: 1)
-//                TextField("단어 개수를 입력하세요", text: $wordsCount, format: .number)
-                Button (action: {}) {
-                    NavigationLink(destination: ContentView(wordsCount: $wordsCount)){
-                        Text("단어 확인")
+//                Slider(value: $wordsCount, in: 0...15, step: 1)
+                HStack {
+                    TextField("1이상 15이하 숫자 입력", value: $wordsCount, format: .number)
+                        .textFieldStyle(.roundedBorder)
+                        .frame(width: 200, alignment: .center)
+                        .padding()
+                    Button (action: {
+                        if wordsCount < 1 || wordsCount > 15 {
+                            showingAlert = true
+                        }
+                    }) {
+                        if wordsCount < 1 || wordsCount > 15 {
+                            Text("단어 확인")
+                        } else {
+                            NavigationLink(destination: ContentView(wordsCount: $wordsCount)){
+                                Text("단어 확인")
+                            }
+                        }
+                    }
+                    .alert("1이상 15 이하의 단어만 입력해주세요!", isPresented: $showingAlert) {
+                        Button("확인", role: .cancel) { }
                     }
                 }
+                
             }
         }
     }
