915bfe272ff32e4e6a86d1f88db2e6f2a3a31049
rkddnr330 <rkddnr330@gmail.com>
2022-06-01 16:19:40 +0900

    [SIWA] initial commit

---
+++ SIWA/SIWA/SIWAApp.swift
@@ -0,0 +1,17 @@
+//
+//  SIWAApp.swift
+//  SIWA
+//
+//  Created by Park Kangwook on 2022/06/01.
+//
+
+import SwiftUI
+
+@main
+struct SIWAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
