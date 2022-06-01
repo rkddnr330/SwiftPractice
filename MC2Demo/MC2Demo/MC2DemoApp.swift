aa9a346f5e53b8a737dabcf64317d7d3f3100d2f
rkddnr330 <rkddnr330@gmail.com>
2022-06-01 21:53:38 +0900

    [MC2Demo] initial commit

---
+++ MC2Demo/MC2Demo/MC2DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MC2DemoApp.swift
+//  MC2Demo
+//
+//  Created by Park Kangwook on 2022/06/01.
+//
+
+import SwiftUI
+
+@main
+struct MC2DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
