f2a563d34df97174797c3294c0efd401ead5d23e
rkddnr330 <rkddnr330@gmail.com>
2022-06-03 22:50:38 +0900

    [ScholarshipDemo] feat: fetchPNUHomeData 작성

--- ScholarshipDemo/ScholarshipDemo/ContentView.swift
+++ ScholarshipDemo/ScholarshipDemo/ContentView.swift
@@ -6,11 +6,36 @@
 //
 
 import SwiftUI
+import SwiftSoup
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Button("Go!") {
+            fetchPNUHomeData()
+        }
+    }
+    
+    var carLabel: String
+    
+    func fetchPNUHomeData() {
+        
+        let urlAddress = "https://www.pusan.ac.kr/kor/CMS/Board/Board.do?mCode=MN095"
+                
+        guard let url = URL(string: urlAddress) else { return }
+        
+        do {
+            let html = try String(contentsOf: url, encoding: .utf8)
+            let doc: Document = try SwiftSoup.parse(html)
+            
+            let title: Elements = try doc.select("stitle").select("a")
+            
+            carLabel = title
+            print(carLabel)
+            
+        } catch let error {
+            print("Error : \(error)")
+        }
+        
     }
 }
 
