a1b9325506bd793ab6f0ba32865f8aaad87174b9
rkddnr330 <rkddnr330@gmail.com>
2022-06-25 22:28:06 +0900

    [ScholarshipDemo2] feat: launch screen

--- ScholarshipDemo2/ScholarshipDemo2/Articles/Views/AppStorageTest.swift
+++ ScholarshipDemo2/ScholarshipDemo2/Articles/Views/AppStorageTest.swift
@@ -9,13 +9,46 @@ import SwiftUI
 
 struct AppStorageTest: View {
     @AppStorage("text") var isOnboardingActive = true
+    
+    @State var isLoading: Bool = true
     var body: some View {
-        if isOnboardingActive {
-            OnboardingDemo(isOnboardingActive: $isOnboardingActive)
-        } else {
-            MoneyView(isOnboardingActive: $isOnboardingActive)
+        ZStack {
+            // 앱 화면
+            if isOnboardingActive {
+                OnboardingDemo(isOnboardingActive: $isOnboardingActive).zIndex(0)
+            } else {
+                MoneyView(isOnboardingActive: $isOnboardingActive).zIndex(0)
+            }
+            
+            // Launch Screen
+            if isLoading {
+                launchScreenView.transition(.opacity).zIndex(1)
+            }
+            
+        }
+        .onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 3, execute: {
+                withAnimation { isLoading.toggle() }
+            })
         }
         
+//        if isOnboardingActive {
+//            OnboardingDemo(isOnboardingActive: $isOnboardingActive)
+//        } else {
+//            MoneyView(isOnboardingActive: $isOnboardingActive)
+//        }
+        
+    }
+}
+
+extension AppStorageTest {
+    var launchScreenView: some View {
+        ZStack(alignment: .center) {
+            LinearGradient(gradient: Gradient(colors: [Color("LogoMain"), Color("LogoSub")]),
+                           startPoint: .top, endPoint: .bottom)
+            .edgesIgnoringSafeArea(.all)
+            Image("splash")
+        }
     }
 }
 
